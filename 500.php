0a7b5dd2339de3390aabc724b29b7e95927ba60b
Steva0 <michele.stevanin@gmail.com>
2025-12-08 18:23:06 +0100

    Corretti footer e impaginazioni

--- 500.php
+++ 500.php
@@ -1,36 +1,8 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="utf-8">
-    <title>Errore Server - Tenuta al Morer</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style/stile.css" media="screen"> 
-    <link rel="stylesheet" href="style/mini.css" media="screen and (max-width:940px)">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-</head>
-<body>
-    <header>
-        <div class="login-header-logo">
-            <a href="index.php"><img src="images/logo.webp" alt="Home - Tenuta al Morer"></a>
-        </div>
-    </header>
+<?php
+require_once 'common.php';
 
-    <main id="content">
-        <div class="auth-container" style="text-align: center;">
-            <i class="fas fa-cogs" style="font-size: 4rem; color: #666; margin-bottom: 20px;"></i>
-            
-            <h1>Qualcosa non va...</h1>
-            <p>Abbiamo un piccolo problema tecnico in "cantina".</p>
-            <p>Stiamo lavorando per risolvere l'errore. Riprova tra qualche minuto.</p>
-            
-            <div style="margin-top: 2em;">
-                <a href="index.php" class="btn-primary">RIPROVA</a>
-            </div>
-        </div>
-    </main>
-    
-    </body>
-</html>
+// Imposta il codice di stato HTTP 500 (Internal Server Error)
+http_response_code(500);
+
+echo caricaPagina('500.html');
+?>
