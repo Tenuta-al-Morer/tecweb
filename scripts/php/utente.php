088eedb7b676cd584f1d68db66c72b5d01f44f2b
Alessandro Contarini <alessandro.contarini21@gmail.com>
2025-12-12 22:24:28 +0100

    test

--- scripts/php/utente.php
+++ scripts/php/utente.php
@@ -15,7 +15,10 @@ $htmlContent = caricaPagina('../../html/utente.html');
 
 
 $emailUtente = htmlspecialchars($_SESSION['utente']);
+$ruoloUtente = $_SESSION['ruolo'];
 $htmlContent = str_replace("[email_utente]", $emailUtente, $htmlContent);
+$htmlContent = str_replace("[riferimento]", $ruoloUtente, $htmlContent);
+
 
 
 echo $htmlContent;
