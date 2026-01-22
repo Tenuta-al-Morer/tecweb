774f690055ede38a4540fca8f648dc2a8a39d81c
Steva0 <michele.stevanin@gmail.com>
2026-01-22 17:21:18 +0100

    Ristrutturata pagina mappa sito

--- docs/scripts/php/mappa.php
+++ docs/scripts/php/mappa.php
@@ -1,91 +1,147 @@
 <?php
 require_once 'common.php';
 
-$ruolo = isset($_SESSION['ruolo']) ? $_SESSION['ruolo'] : null;
-$isLogged = isset($_SESSION['utente']);
+// Struttura completa del sito 
+$struttura = [
+    'Home' => [
+        'link' => 'home.php',
+        'lang' => 'en',
+        'breadcrumb' => ['Home'],
+        'sub' => [
+            'Tenuta' => [
+                'link' => 'tenuta.php',
+                'breadcrumb' => ['Home', 'Tenuta']
+            ],
+            'Vini' => [
+                'link' => 'vini.php',
+                'breadcrumb' => ['Home', 'Vini']
+            ],
+            'Esperienze' => [
+                'link' => 'esperienze.php',
+                'breadcrumb' => ['Home', 'Esperienze']
+            ],
+            'Contatti' => [
+                'link' => 'contatti.php',
+                'breadcrumb' => ['Home', 'Contatti']
+            ],
+            'Carrello' => [
+                'link' => 'carrello.php',
+                'breadcrumb' => ['Home', 'Carrello'],
+                'sub' => [
+                    'Checkout' => [
+                        'link' => 'checkout.php',
+                        'lang' => 'en',
+                        'breadcrumb' => ['Home', 'Carrello', 'Spedizione']
+                    ]
+                ]
+            ],
+            'Login' => [
+                'link' => 'login.php',
+                'lang' => 'en',
+                'breadcrumb' => ['Home', 'Login'],
+                'sub' => [
+                    'Registrazione' => [
+                        'link' => 'registrazione.php',
+                        'breadcrumb' => ['Home', 'Login', 'Registrazione']
+                    ]
+                ]
+            ],
+            'Area riservata' => [
+                'link' => 'areaPersonale.php',
+                'breadcrumb' => ['Home', 'Area Riservata']
+            ],
+            'Area gestionale' => [
+                'link' => 'gestionale.php',
+                'breadcrumb' => ['Home', 'Area Gestionale'],
+                'sub' => [
+                    'Ordini' => ['link' => 'gestionale.php?sezione=ordini'],
+                    'Richieste esperienze' => ['link' => 'gestionale.php?sezione=esperienze'],
+                    'Messaggi clienti' => ['link' => 'gestionale.php?sezione=messaggi']
+                ]
+            ],
+            'Amministrazione' => [
+                'link' => 'admin.php',
+                'breadcrumb' => ['Home', 'Amministrazione'],
+                'sub' => [
+                    'Gestione vini' => ['link' => 'admin.php?view=vini'],
+                    'Gestione utenti' => ['link' => 'admin.php?view=utenti']
+                ]
+            ],
+            'Note legali' => [
+                'link' => 'policy.php',
+                'breadcrumb' => ['Home', 'Policy'],
+                'sub' => [
+                    'Privacy Policy' => [
+                        'link' => 'policy.php#privacy-policy', 
+                        'lang' => 'en',
+                        'breadcrumb' => ['Home', 'Policy', 'Privacy Policy']
+                    ],
+                    'Accessibilità' => [
+                        'link' => 'policy.php#accessibility',
+                        'breadcrumb' => ['Home', 'Policy', 'Accessibilità']
+                    ]
+                ]
+            ],
+            'Mappa del sito' => [
+                'link' => 'mappa.php',
+                'breadcrumb' => ['Home', 'Mappa del sito']
+            ]
+        ]
+    ]
+];
 
-$sitemapTree = '
-<ul class="sitemap-tree-root">
-    <li>
-        <a href="home.php"><span lang="en">Home</span></a>
-        <ul>
-            <li><a href="tenuta.php">Tenuta</a></li>
-            <li><a href="vini.php">Vini</a></li>
-            <li><a href="esperienze.php">Esperienze</a></li>
-            <li><a href="contatti.php">Contatti</a></li>
-            <li>
-                <a href="carrello.php">Carrello</a>';
+/* Funzione ricorsiva per generare la mappa HTML */
+function generaListaHTML($items, $livello = 0) {
+    if (empty($items)) return '';
+    
+    $classeLivello = 'level-' . min($livello, 3);
+    
+    $html = '<ul class="tree-list ' . $classeLivello . '">';
+    
+    foreach ($items as $label => $data) {
+        $link = $data['link'] ?? '#';
+        $sub = $data['sub'] ?? [];
+        $langAttr = isset($data['lang']) ? ' <span lang="'.$data['lang'].'">' : '';
+        $langClose = isset($data['lang']) ? '</span>' : '';
+        $breadcrumb = isset($data['breadcrumb']) ? ' title="Percorso: ' . implode(' &gt; ', $data['breadcrumb']) . '"' : '';
+        
+        $classeItem = ($livello > 0) ? ' class="child-item"' : '';
 
-if ($ruolo === 'user') {
-    $sitemapTree .= '
-                <ul>
-                    <li><a href="checkout.php"><span lang="en">Checkout</span></a></li>
-                </ul>';
+        $html .= '<li' . $classeItem . '>';
+        $html .= '<a href="' . htmlspecialchars($link) . '"' . $breadcrumb . '>' . 
+                 $langAttr . htmlspecialchars($label) . $langClose . '</a>';
+        
+        if (!empty($sub)) {
+            $html .= generaListaHTML($sub, $livello + 1);
+        }
+        
+        $html .= '</li>';
+    }
+    
+    $html .= '</ul>';
+    return $html;
 }
 
-$sitemapTree .= '
-            </li>';
-
-if ($ruolo === 'user') {
-    $sitemapTree .= '
-            <li><a href="areaPersonale.php">Area riservata</a></li>';
+function countTotalPages($items) {
+    $count = 0;
+    foreach ($items as $item) {
+        $count++;
+        if (isset($item['sub'])) {
+            $count += countTotalPages($item['sub']);
+        }
+    }
+    return $count;
 }
 
-if ($ruolo === 'staff') {
-    $sitemapTree .= '
-            <li>
-                <a href="gestionale.php">Area gestionale</a>
-                <ul>
-                    <li><a href="gestionale.php?sezione=ordini">Ordini</a></li>
-                    <li><a href="gestionale.php?sezione=esperienze">Richieste esperienze</a></li>
-                    <li><a href="gestionale.php?sezione=messaggi">Messaggi clienti</a></li>
-                </ul>
-            </li>';
-}
+$sitemapHTML = generaListaHTML($struttura);
 
-if ($ruolo === 'admin') {
-    $sitemapTree .= '
-            <li>
-                <a href="gestionale.php">Area gestionale</a>
-                <ul>
-                    <li><a href="gestionale.php?sezione=ordini">Ordini</a></li>
-                    <li><a href="gestionale.php?sezione=esperienze">Richieste esperienze</a></li>
-                    <li><a href="gestionale.php?sezione=messaggi">Messaggi clienti</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="admin.php">Amministrazione</a>
-                <ul>
-                    <li><a href="admin.php?view=vini">Gestione vini</a></li>
-                    <li><a href="admin.php?view=utenti">Gestione utenti</a></li>
-                </ul>
-            </li>';
-}
+$totalePagine = count($struttura['Home']['sub']);
+$totaleSezioni = countTotalPages($struttura);
 
-if (!$isLogged) {
-    $sitemapTree .= '
-            <li>
-                <a href="login.php"><span lang="en">Login</span></a>
-                <ul>
-                    <li><a href="registrazione.php">Registrazione</a></li>
-                </ul>
-            </li>';
-}
-
-$sitemapTree .= '
-            <li>
-                <a href="policy.php">Note legali</a>
-                <ul>
-                    <li><a href="policy.php#privacy-policy"><span lang="en">Privacy Policy</span></a></li>
-                    <li><a href="policy.php#accessibility">Accessibilita</a></li>
-                </ul>
-            </li>
-            <li><a href="mappa.php">Mappa del sito</a></li>
-        </ul>
-    </li>
-</ul>';
 
 echo caricaPagina('../../html/mappa.html', [
-    '[sitemap_tree]' => $sitemapTree
+    '[sitemap_content]' => $sitemapHTML,  
+    '[totale_pagine]'   => $totalePagine, 
+    '[totale_sezioni]'  => $totaleSezioni 
 ]);
 ?>
